eba8533f2a7f341435015d4a78e9d5494d85f8b7
vladimir-github-account <volodymyr.dashkevychit@gmail.com>
2022-04-01 08:41:27 +0300

    Add: responsive desktop & tablet

--- src/components/InfoContainer/index.js
+++ src/components/InfoContainer/index.js
@@ -3,12 +3,14 @@ import styles from './InfoContainer.module.sass';
 import Button from "../Button";
 
 const InfoContainer = () => {
-    const {infoContainer, mainHeader, info} = styles;
+    const {infoContainer, mainHeader, info, wrapper} = styles;
     return (
         <div className={infoContainer}>
             <h1 className={mainHeader}>COVID-19 Vaccine Updates</h1>
-            <p className={info}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque debitis hic possimus quasi. Blanditiis cupiditate eligendi facere minus odio recusandae, sint temporibus!</p>
-            <Button/>
+            <div className={wrapper}>
+                <p className={info}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque debitis hic possimus quasi. Blanditiis cupiditate eligendi facere minus odio recusandae, sint temporibus!</p>
+                <Button/>
+            </div>
         </div>
 
     );
